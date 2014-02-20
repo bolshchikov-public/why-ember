5d5f37c85dbf51e964d732e633f036e9fd2e9474
bolshchikov <sergey@bolshchikov.net>
2014-02-20 23:56:53 +0200

    jquery setup added

--- src/js/setup-jquery.js
+++ src/js/setup-jquery.js
@@ -3,3 +3,28 @@
  * @fileOverview
  * @author sergey
  */
+
+'use strict';
+
+var App = {
+      user: {}
+    },
+    firstNameInput = $('#firstName'),
+    lastNameInput = $('#lastName'),
+    fullNameInput = $('#fullName');
+
+// set initial values
+fullNameInput.val(App.user.firstName + ' ' + App.user.lastName);
+
+// bind events
+firstNameInput.on('keyup', function () {
+  App.user.firstName = firstNameInput.val();
+  fullNameInput.val(firstNameInput.val() + ' ' + lastNameInput.val());
+});
+
+lastNameInput.on('keyup', function () {
+  App.user.lastName = lastNameInput.val();
+  fullNameInput.val(firstNameInput.val() + ' ' + lastNameInput.val());
+});
+
+window.App = App;
